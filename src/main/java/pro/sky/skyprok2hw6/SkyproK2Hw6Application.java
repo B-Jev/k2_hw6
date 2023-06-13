032789b09d31b2e3bc781e5aee2d58f36495f807
B-Jev <baldenmax@gmail.com>
2023-06-13 22:08:49 +0300

    Введение в Maven и Spring Boot.

---
+++ src/main/java/pro/sky/skyprok2hw6/SkyproK2Hw6Application.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprok2hw6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproK2Hw6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproK2Hw6Application.class, args);
+	}
+
+}
